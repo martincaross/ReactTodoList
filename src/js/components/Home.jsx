1abd01b36998c72162b60d28b638d1d386804034
Martín <martinjcaro@gmail.com>
2025-03-23 22:20:56 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from 'react';
+// index.css'
+import "../../styles/index.css";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState('');
+  
+	const handleChange = (e) => {
+	  setNewTask(e.target.value);
+	};
+  
+	const handleKeyDown = (e) => {
+	  if (e.key === 'Enter' && newTask.trim() !== '') {
+		setTasks([...tasks, newTask]);
+		setNewTask('');
+	  }
+	};
+  
+	const deleteTask = (index) => {
+	  const updatedTasks = tasks.filter((_, i) => i !== index);
+	  setTasks(updatedTasks);
+	};
+  
 	return (
-		<div className="text-center">
-            
+		<>
+			<h1>To-Do List</h1>
+			<div className="container">
+			<input
+				type="text"
+				placeholder="Añadir nueva tarea"
+				value={newTask}
+				onChange={handleChange}
+				onKeyDown={handleKeyDown}
+			/>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<ul>
+				{tasks.length === 0 ? (
+				<li className="empty">No hay tareas, añadir tareas</li>
+				) : (
+				tasks.map((task, index) => (
+					<li key={index}>
+					{task}
+					<span className="delete" onClick={() => deleteTask(index)}>
+						X
+					</span>
+					</li>
+				))
+				)}
+			</ul>
+			</div>
+		</>
 	);
-};
-
-export default Home;
+  };
+  
+  export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,6 +11,8 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
